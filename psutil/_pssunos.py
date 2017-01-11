e9ca72ead29c2ea454000e5c8b4ae0406a072374
Krzysztof Płocharz <plocharz@9livesdata.com>
2017-01-11 11:13:45 +0100

    Fix _parse_pargs_output to work in Python 3 (#1)

--- psutil/_pssunos.py
+++ psutil/_pssunos.py
@@ -422,8 +422,10 @@ class Process(object):
     def cmdline(self):
         cmd = [self.PARGS_BINARY, str(self.pid)]
         try:
-            out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-            return self._parse_pargs_output(out)
+            stdout = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+            if PY3:
+                stdout = stdout.decode(sys.stdout.encoding)
+            return self._parse_pargs_output(stdout)
         except (subprocess.CalledProcessError, EnvironmentError):
             return self._proc_name_and_args()[1].split(' ')
 
